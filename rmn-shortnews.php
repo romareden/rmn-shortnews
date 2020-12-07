05370075a0e1d6daa90bc400833d1e841cbb4bf2
Roman Seredenko <roman.seredenko@gmail.com>
2020-12-07 20:54:35 +0200

    Initial. Folder and one file.

---
+++ rmn-shortnews.php
@@ -0,0 +1,9 @@
+/*
+	Plugin Name: Short News
+	Plugin URI:
+	Description: Add custom post type for short news
+	Version: 0.0.1
+	Author: Roman Seredenko
+	Author URI: https://romans.website
+	License: GPLv2
+*/
